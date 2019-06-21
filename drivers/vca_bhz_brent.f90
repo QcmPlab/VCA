d5a450ad9c942daa99eae3897757ea31dcc8af08
lcrippa <crippa.lorenzo@gmail.com>
2019-06-21 12:48:12 +0200

    update CMake

--- drivers/vca_bhz_brent.f90
+++ drivers/vca_bhz_brent.f90
@@ -38,7 +38,7 @@ program vca_bhz_2d
   real(8),dimension(:),allocatable                :: ts_array_x,ts_array_y,params
   real(8),dimension(:,:),allocatable              :: omega_grid
   real(8),allocatable,dimension(:,:)              :: kgrid_test,kpath_test
-  real(8),dimension(3)                            :: df
+    real(8),dimension(3)                      :: df
   !
   !MPI INIT
   !
@@ -120,11 +120,11 @@ program vca_bhz_2d
     !
     params=[t,M,lambda]
     !
-    call minimize_parameters(params,0.5d0)
-    !call fmin_brent(params,0.2d0)
+    !call minimize_parameters(params,1.d0)
+    call fmin_brent(params,1.0d0)
     !
-    print_Sigma=.true.
     print_observables=.true.
+    print_Sigma=.true.
     omegadummy=solve_vca_multi(params)
     !
     write(*,"(A,F15.9,A,3F15.9)")bold_green("FOUND STATIONARY POINT "),omegadummy,bold_green(" AT "),t_var,m_var,lambda_var
@@ -139,15 +139,13 @@ program vca_bhz_2d
     allocate(ts_array_y(Nloop))
     allocate(omega_grid(Nloop,Nloop))
     !
-    ts_array_x = linspace(ts_var-0.2d0,ts_var+0.2d0,Nloop)
-    ts_array_y = linspace(Mh_var-0.2d0,Mh_var+0.2d0,Nloop)
+    ts_array_x = linspace(1.0d0,3.5d0,Nloop)
+    ts_array_y = linspace(0.d0,Mh_var+1.d0,Nloop)
     do iloop=1,Nloop
-      do jloop=1,Nloop
-        omega_grid(iloop,jloop)=solve_vca_multi([ts_array_x(iloop),ts_array_y(jloop),lambda])
-      enddo
+        omega_grid(iloop,1)=solve_vca_multi([ts_array_x(iloop),Mh_var,lambda])
     enddo
     !
-    call splot3d("sft_Omega_loopVSts.dat",ts_array_x,ts_array_y,omega_grid)
+    call splot("sft_Omega_loopVSts.dat",ts_array_x,omega_grid(:,1))
     !
   else
     print_observables=.true.
@@ -337,7 +335,7 @@ contains
     !
     do i=1,size(v)
       nbd(i) = 2
-      l(i)   = parvec(i)-radius
+      l(i)   = 0.d0
       u(i)   = parvec(i)+radius
     enddo
     lold=l
@@ -398,33 +396,36 @@ contains
     !
     print*,"MINIMIZE T"
     !
-    call  brent(solve_vca_t,v(1),[t_var-radius,t_var+radius])   
-    if((abs(t_var-v(1)-radius) .lt. 1.d-5) .or. (abs(t_var-v(1)+radius) .lt. 1.d-5) )then
+    call  brent_(solve_vca_t,v(1),[0.2d0,t_var+radius])   
+    if((abs(t_var) .lt. 1.d-5) .or. (abs(t_var-v(1)-radius) .lt. 1.d-5) )then
       MULTIMAX=.true.
-      call  brent(solve_vca_t,v(1),[t_var-radius,t_var+radius])
-      if((abs(t_var-v(1)-radius) .lt. 1.d-5) .or. (abs(t_var-v(1)+radius) .lt. 1.d-5) )STOP "error on minimizing t"
+      print*,"MAXIMIZE T"
+      call  brent_(solve_vca_t,v(1),[0.2d0,t_var+radius])
+      if((abs(t_var) .lt. 1.d-5) .or. (abs(t_var-v(1)-radius) .lt. 1.d-5) )STOP "error on minimizing t"
     endif
     t_var=v(1)
     !
     print*,"MINIMIZE M"
     !
     MULTIMAX=.true.
-    call  brent(solve_vca_m,v(2),[M_var-radius,M_var+radius])   
-    if((abs(M_var-v(2)-radius) .lt. 1.d-5) .or. (abs(M_var-v(2)+radius) .lt. 1.d-5) )then
+    call  brent_(solve_vca_m,v(2),[0.2d0,M_var+radius])   
+    if((abs(M_var) .lt. 1.d-5) .or. (abs(M_var-v(2)-radius) .lt. 1.d-5) )then
       MULTIMAX=.false.
-      call  brent(solve_vca_m,v(2),[M_var-radius,M_var+radius])
-      if((abs(M_var-v(2)-radius) .lt. 1.d-5) .or. (abs(M_var-v(2)+radius) .lt. 1.d-5) )STOP "error on minimizing t"
+      print*,"MAXIMIZE M"
+      call  brent_(solve_vca_m,v(2),[0.2d0,M_var+radius])
+      if((abs(M_var) .lt. 1.d-5) .or. (abs(M_var-v(2)-radius) .lt. 1.d-5) )STOP "error on minimizing t"
     endif
     M_var=v(2)
     !
     print*,"MINIMIZE LAMBDA"
     !
     MULTIMAX=.true.
-    call  brent(solve_vca_l,v(3),[lambda_var-radius,lambda_var+radius])   
-    if((abs(lambda_var-v(3)-radius) .lt. 1.d-5) .or. (abs(lambda_var-v(3)+radius) .lt. 1.d-5) )then
+    call  brent_(solve_vca_l,v(3),[0.2d0,lambda_var+radius])   
+    if((abs(lambda_var) .lt. 1.d-5) .or. (abs(lambda_var-v(3)-radius) .lt. 1.d-5) )then
       MULTIMAX=.false.
-      call  brent(solve_vca_l,v(3),[lambda_var-radius,lambda_var+radius])
-      if((abs(lambda_var-v(3)-radius) .lt. 1.d-5) .or. (abs(lambda_var-v(3)+radius) .lt. 1.d-5) )STOP "error on minimizing t"
+      print*,"MAXIMIZE LAMBDA"
+      call  brent_(solve_vca_l,v(3),[0.2d0,lambda_var+radius])
+      if((abs(lambda_var) .lt. 1.d-5) .or. (abs(lambda_var-v(3)-radius) .lt. 1.d-5) )STOP "error on minimizing t"
     endif
     lambda_var=v(3)
     MULTIMAX=.false.
@@ -1223,6 +1224,142 @@ contains
     indices(2)=mod(N,Nx)+1
     indices(1)=N/Nx+1
   end function N2indices
+
+
+  subroutine brent_(func,xmin,brack,tol,niter)
+    interface
+       function func(x)
+         real(8) :: x
+         real(8) :: func
+       end function func
+    end interface
+    real(8),intent(inout)         :: xmin
+    real(8),dimension(:),optional :: brack
+    real(8),optional              :: tol
+    integer,optional              :: niter
+    real(8)                       :: tol_
+    integer                       :: niter_
+    integer                       :: iter
+    real(8)                       :: ax,xx,bx,fa,fx,fb,fret
+    !
+    tol_=1d-9;if(present(tol))tol_=tol
+    Niter_=200;if(present(Niter))Niter_=Niter
+    !
+    if(present(brack))then
+       select case(size(brack))
+       case(1)
+          stop "Brent error: calling brent with size(brack)==1. None or two points are necessary."
+       case(2)
+          ax = brack(1)
+          xx = brack(2)
+       case (3)
+          ax = brack(1)
+          xx = brack(2)
+          bx = brack(3)
+       end select
+    else
+       ax=0d0
+       xx=1d0
+    endif
+    fret=brent_optimize(ax,xx,bx,func,tol_,niter_,xmin)
+  end subroutine brent_
+  !
+
+
+
+  function brent_optimize(ax,bx,cx,func,tol,itmax,xmin)
+    real(8), intent(in)  :: ax,bx,cx,tol
+    real(8), intent(out) :: xmin
+    real(8)              :: brent_optimize
+    integer              :: itmax
+    real(8), parameter   :: cgold=0.3819660d0,zeps=1.0d-3*epsilon(ax)
+    integer              :: iter
+    real(8)              :: a,b,d,e,etemp,fu,fv,fw,fx,p,q,r,tol1,tol2,u,v,w,x,xm
+    interface
+       function func(x)
+         real(8) :: x
+         real(8) :: func
+       end function func
+    end interface
+    a=min(ax,cx)
+    b=max(ax,cx)
+    v=bx
+    w=v
+    x=v
+    e=0.d0
+    fx=func(x)
+    fv=fx
+    fw=fx
+    do iter=1,itmax
+       xm=0.5d0*(a+b)
+       tol1=tol*abs(x)+zeps
+       tol2=2.0*tol1
+       if (abs(x-xm) <= (tol2-0.5d0*(b-a))) then
+          xmin=x
+          brent_optimize=fx
+          return
+       end if
+       if (abs(e) > tol1) then
+          r=(x-w)*(fx-fv)
+          q=(x-v)*(fx-fw)
+          p=(x-v)*q-(x-w)*r
+          q=2.d0*(q-r)
+          if (q > 0.d0) p=-p
+          q=abs(q)
+          etemp=e
+          e=d
+          if (abs(p) >= abs(0.5d0*q*etemp) .or. &
+               p <= q*(a-x) .or. p >= q*(b-x)) then
+             e=merge(a-x,b-x, x >= xm )
+             d=cgold*e
+          else
+             d=p/q
+             u=x+d
+             if (u-a < tol2 .or. b-u < tol2) d=sign(tol1,xm-x)
+          end if
+       else
+          e=merge(a-x,b-x, x >= xm )
+          d=cgold*e
+       end if
+       u=merge(x+d,x+sign(tol1,d), abs(d) >= tol1 )
+       fu=func(u)
+       if (fu <= fx) then
+          if (u >= x) then
+             a=x
+          else
+             b=x
+          end if
+          call shft(v,w,x,u)
+          call shft(fv,fw,fx,fu)
+       else
+          if (u < x) then
+             a=u
+          else
+             b=u
+          end if
+          if (fu <= fw .or. w == x) then
+             v=w
+             fv=fw
+             w=u
+             fw=fu
+          else if (fu <= fv .or. v == x .or. v == w) then
+             v=u
+             fv=fu
+          end if
+       end if
+    end do
+    !pause 'brent: exceed maximum iterations'
+
+  end function brent_optimize
+
+    subroutine shft(a,b,c,d)
+      real(8), intent(out) :: a
+      real(8), intent(inout) :: b,c
+      real(8), intent(in) :: d
+      a=b
+      b=c
+      c=d
+    end subroutine shft
 
 
 end program vca_bhz_2d
